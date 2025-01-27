217a94c70f34bd1317ac01671d190abb644ebe0c
ashrafhamil <ashraf@borong.com>
2025-01-27 09:19:58 +0800

    feat: add vercel analytics to track traffice

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import './style.css';
+import { inject } from '@vercel/analytics';
+
+// if (import.meta.env.PROD) {
+//     inject(); // Enable only in production
+// }
+inject();
 
 const app = createApp(App);
 app.use(createPinia());
